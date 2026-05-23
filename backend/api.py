169a3de3e6ed2aada55ea81a92601c24285b1204
Jachdich <jamesedkitching@gmail.com>
2026-05-23 14:57:24 +0100

    first commit

---
+++ backend/api.py
@@ -0,0 +1,62 @@
+from main import ingest_signups, read_excel
+import main
+from flask import Flask, flash, request, redirect, url_for
+import flask
+#meow
+
+import sqlite3
+from flask import g
+
+DATABASE = 'eugc.db'
+app = Flask(__name__, static_folder="../frontend", static_url_path="/static")
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@app.route('/api/v1/add_signups', methods=['POST'])
+def upload_file():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return flask.Response(status=400)
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file:
+        table = read_excel(file.stream)
+        print("adding", table)
+        db = get_db()
+        ingest_signups(db, table)
+        return flask.Response(status=200)
+
+# TODO this may perform horribly if the number of users grows
+@app.route("/api/v1/list_people", methods=["GET"])
+def list_people():
+    db = get_db()
+    people = main.list_people(db)
+    return [{
+        "id": person.id,
+        "name": person.name,
+        "e_number": person.e_number,
+        "emails": person.emails,
+        "phones": person.phones,
+        "num_signups": main.num_signups(db, person.id),
+        "num_flying_days": main.num_flying_days(db, person.id),
+        "keenness": person.keenness,
+        "briefing_score": person.briefing_score,
+        "briefing_date": person.briefing_date.timestamp() if person.briefing_date is not None else None,
+    } for person in [main.person_info(db, p) for p in people]]
+
+app.run()
---
+++ backend/main.py
@@ -0,0 +1,321 @@
+import sqlite3
+import flask
+import csv
+from io import BytesIO
+import openpyxl
+import datetime
+from dataclasses import dataclass
+
+# How to make it easy to make new memebership numbers - can't just be me being able to, automate the process or make it easy for anyone, document it
+# In general we need better account logging - who pays for what flights, did they actually pay, E0000 costs broken down, mx, etc - internal use, record keeping
+# improved planning of weekly flying
+
+# TODO
+# warn about double entry
+# people signing up twice a week
+# handle multiple/changing emails/phones
+
+con = sqlite3.connect("eugc.db")
+cur = con.cursor()
+
+cur.execute("""
+create table if not exists signups (
+    id integer primary key,
+    person integer not null,
+    completed_datetime integer not null,
+    available_days integer not null,
+    notes text,
+    foreign key (person) references people(id)
+)""")
+
+cur.execute("""
+create table if not exists flying_days (
+    id integer primary key,
+    date integer not null,
+    instruct integer not null,
+    drive integer not null,
+    supervise integer not null,
+    notes text,
+    foreign key (instruct) references people(id),
+    foreign key (drive) references people(id),
+    foreign key (supervise) references people(id)
+)""")
+
+cur.execute("""
+create table if not exists flying_days_people (
+    id integer primary key,
+    flying_day integer not null,
+    person integer not null,
+    foreign key (person) references people(id),
+    foreign key (flying_day) references flying_days(id),
+    unique (flying_day, person)
+)""")
+
+cur.execute("""
+create table if not exists people (
+    id integer primary key,
+    name text not null unique,
+    has_car boolean,
+    e_number integer unique,
+    keenness real,
+    role text,
+    notes text
+)""")
+
+cur.execute("""
+create table if not exists emails (
+    id integer primary key,
+    person integer not null,
+    email text unique,
+    foreign key (person) references people(id)
+)
+""")
+
+cur.execute("""
+create table if not exists phones (
+    id integer primary key,
+    person integer not null,
+    phone text unique,
+    foreign key (person) references people(id)
+)
+""")
+
+cur.execute("""
+create table if not exists briefings (
+    id integer primary key,
+    date integer not null,
+    person integer not null,
+    score real not null,
+    foreign key (person) references people(id)
+)""")
+
+@dataclass
+class Person:
+    id: int
+    name: str
+    emails: list[str]
+    phones: list[str]
+    has_car: bool | None
+    e_number: int | None
+    keenness: float | None
+    role: str | None
+    notes: str | None
+
+    briefing_date: datetime.datetime | None
+    briefing_score: float | None
+
+con.commit()
+
+def read_excel(stream) -> list[tuple[any]]:
+    # f = BytesIO(bytes_)
+    workbook = openpyxl.load_workbook(stream)
+    sheet = workbook.active
+
+    table = list(sheet.values)
+    return table
+            
+def find_person_by_details(db, name, email, phone) -> int | None:
+    cur = db.cursor()
+    by_name = list(cur.execute("select id from people where name = ?", (name,)))
+    if len(by_name) == 1:
+        return by_name[0][0]
+    by_phone = list(cur.execute("select person from phones where phone = ?", (phone,)))
+    if len(by_phone) == 1:
+        return by_phone[0][0]
+    by_email = list(cur.execute("select person from emails where email = ?", (email,)))
+    if len(by_email) == 1:
+        return by_email[0][0]
+
+    # can't find it
+    # TODO search similar names?
+    return None
+
+MONDAY   = 0b0000001
+TUESDAY  = 0b0000010
+WEDNESDAY= 0b0000100
+THURSDAY = 0b0001000
+FRIDAY   = 0b0010000
+SATURDAY = 0b0100000
+SUNDAY   = 0b1000000
+
+def ingest_signups(db, data: list[tuple[any]]):
+    cur = db.cursor()
+    header = data[0]
+    body = data[1:]
+    if all(test in colname for colname, test in zip(header, ('ID', 'Start time', 'Completion time', 'Email', 'Name', 'Is this your trial flight?', "Do you plan to attend this week's trial flight briefing?", 'Please enter your full name', 'Which days are you available?', 'Notes/comments (optional)', 'Email Addresss', 'Mobile number (in case we need to call you)', 'Do you have a car (and are willing to help with transport)?', 'Last modified time'))):
+        indices = {
+            "start": 1, "trial": 5, "name": 7, "days": 8, "notes": 9, "email": 10, "phone": 11, "car": 12
+        }
+    elif header == ('ID', 'Start time', 'Completion time', 'Email', 'Name', 'Is this your trial flight?', 'Please enter your full name', 'Which days are you available?', 'Notes/comments (optional)', 'Email Addresss', 'Mobile number (in case we need to call you)', 'Do you have a car (and are willing to help with transport)?', 'Last modified time'):
+        indices = {
+            "start": 1, "trial": 5, "name": 6, "days": 7, "notes": 8, "email": 9, "phone": 10, "car": 11
+        }
+    else:
+        raise ValueError(f"I don't know how to process header {header}")
+    for row in body:
+        start = row[indices["start"]]
+        reported_trial = row[indices["trial"]]
+        name = row[indices["name"]]
+        days = row[indices["days"]]
+        notes = row[indices["notes"]]
+        email = row[indices["email"]]
+        phone = row[indices["phone"]]
+        car = row[indices["car"]] == "Yes"
+
+        if days is None or name is None or email is None or phone is None:
+            print(f"something wrong with {name=} {days=} {email=} {phone=}")
+            continue # TODO warn
+        name = name.strip()
+        email = email.strip()
+        phone = phone.strip()
+        days = days.lower()
+        # Available days: bitfield. bit 0 is monday, 1 is tuesday, etc. 0bssftwtm
+        available_days = 0
+        if "friday" in days:
+            available_days |= FRIDAY
+        if "saturday" in days:
+            available_days |= SATURDAY
+        if "sunday" in days:
+            available_days |= SUNDAY
+
+        person_id = find_person_by_details(db, name, email, phone)
+        if person_id is None:
+            person_id = next(cur.execute("insert into people (name, has_car) values (?, ?) returning id", (name, car)))[0]
+            cur.execute("insert into emails (person, email) values (?, ?)", (person_id, email))
+            cur.execute("insert into phones (person, phone) values (?, ?)", (person_id, phone))
+
+        cur.execute(
+            "insert into signups (person, completed_datetime, available_days, notes) values (?, ?, ?, ?)",
+            (person_id, start.timestamp(), available_days, notes)
+        )
+
+    db.commit()
+        
+def people_available(db, day: int) -> list[int]:
+    cur = db.cursor()
+    now = datetime.datetime.now()
+    monday = now - datetime.timedelta(days=now.weekday())
+    return [i[0] for i in cur.execute("select person from signups where available_days & ? and completed_datetime > ?", (day, monday.timestamp()))]
+
+def num_signups(db, person):
+    cur = db.cursor()
+    return next(cur.execute("select count(1) from signups where person = ?1 limit 1", (person,)))[0]
+
+# SELECT
+#   Name,
+#   Title
+# FROM
+#   artists
+#   LEFT JOIN albums ON artists.ArtistId = albums.ArtistId
+
+def person_info(db, person_id: int) -> Person | None:
+    cur = db.cursor()
+    result = list(cur.execute("select * from people left join briefings on people.id = briefings.person where people.id = ?", (person_id,)))
+    if len(result) == 0:
+        return None
+    assert not (len(result) > 1), "More than one person for the same ID"
+    p = result[0]
+    emails = [i[0] for i in cur.execute("select email from emails where person = ?", (p[0],))]
+    phones = [i[0] for i in cur.execute("select phone from phones where person = ?", (p[0],))]
+    return Person(p[0], p[1], emails, phones, p[2], p[3], p[4], p[5], p[6], datetime.datetime.fromtimestamp(p[8]) if p[8] is not None else None, p[7])
+
+def num_flying_days(db, person):
+    cur = db.cursor()
+    return next(cur.execute("select count(1) from flying_days_people where person = ?1 limit 1", (person,)))[0]
+
+def last_flying_days(db, person):
+    cur = db.cursor()
+    pass
+    
+def add_flying_day(db, date: datetime.datetime, instruct_id: int, drive_id: int, supervise_id: int, notes: str | None, people: list[int]):
+    cur = db.cursor()
+    day_id = next(cur.execute("insert into flying_days (date, instruct, drive, supervise, notes) values (?, ?, ?, ?, ?)", (date.timestamp(), instruct_id, drive_id, supervise_id, notes)))[0]
+    for person in people:
+        cur.execute("insert into flying_days_people (flying_day, person) values (?, ?)", (day_id, person))
+    db.commit()
+
+def add_briefing(db, date: datetime.datetime, people_scores: list[(int, float)]):
+    cur = db.cursor()
+    cur.executemany("insert into briefings (person, date, score) values (?, ?, ?)", [(person, date.timestamp(), score) for person, score in people_scores])
+    db.commit()
+
+flying_days = [next(cur.execute("insert into flying_days (date, instruct, drive, supervise) values (?, ?, ?, ?) returning id", (0, 0, 0, 0)))[0] for _ in range(50)]
+
+with open("flying_list.csv") as f:
+    r = csv.reader(f)
+    next(r)
+    for row in r:
+        name, e, cng, paid, email, phone, signups, flying, keenness, briefing_score, briefing_date, notes, _, _, _, _ = row
+        briefing_score = float(briefing_score) if briefing_score != "" else None
+        keenness = float(keenness) if keenness != "" else None
+        briefing_date = datetime.datetime.strptime(briefing_date, "%d/%m/%Y") if briefing_date != "" else None
+        notes = notes if notes != "" else None
+        e = int(e[1:]) if e != "" else None
+        signups = int(signups)
+        flying = int(flying)
+        name = (" ".join(reversed(name.split(",")))).strip()
+        emails = email.split(" ")
+        
+        # print(f"{name!r}", e, emails, phone, signups, flying, keenness, briefing_score, briefing_date, notes)
+
+        person_id = next(cur.execute("insert into people (name, e_number, keenness, notes) values (?, ?, ?, ?) returning id", (name, e, keenness, notes)))[0]
+        for email in emails:
+            cur.execute("insert into emails (person, email) values (?, ?)", (person_id, email))
+
+        if phone != "":
+            cur.execute("insert into phones (person, phone) values (?, ?)", (person_id, phone))
+        for i in range(signups):
+            cur.execute("insert into signups (person, completed_datetime, available_days, notes) values (?, ?, ?, ?)", (person_id, 0, 0, None))
+        for i in range(flying):
+            cur.execute("insert into flying_days_people (flying_day, person) values (?, ?)", (flying_days[i], person_id))
+
+        if briefing_date is not None and briefing_score is not None:
+            add_briefing(con, briefing_date, [(person_id, briefing_score)])
+            
+
+paths = [
+ # "Edinburgh University Gliding Club Sem2 2025_2026(1-7).xlsx",
+ # "Edinburgh University Gliding Club Sem2 2025_2026(1-8).xlsx",
+]
+
+for path in paths:
+    with open("/home/james/Downloads/"+path, "rb") as f:
+        # data = f.read()
+        ingest_signups(read_excel(f))
+
+# add_briefing(datetime.datetime(2025, 12, 4), [(1, 3), (2, 3), (3, 1)])
+
+# for person in people_available(SUNDAY):
+#     info = person_info(person)
+#     print(info,num_signups(person))
+
+def list_people(db):
+    cur = db.cursor()
+    return [i[0] for i in cur.execute("select id from people")]
+
+cols = [[] for _ in range(9)]
+for id in [i[0] for i in cur.execute("select id from people order by (select count(1) from signups where person = people.id)")]:
+    info = person_info(con, id)
+    print(info)
+    cols[0].append(str(info.name))
+    cols[1].append(("E" + str(info.e_number)) if info.e_number is not None else "")
+    cols[2].append(", ".join(info.emails))
+    cols[3].append(", ".join(info.phones))
+    cols[4].append(str(num_signups(con, id)))
+    cols[5].append(str(num_flying_days(con, id)))
+    cols[6].append(str(info.keenness) if info.keenness is not None else "")
+    cols[7].append(str(info.briefing_score) if info.briefing_score is not None else "")
+    cols[8].append(str(info.briefing_date.date()) if info.briefing_date is not None else "")
+
+
+col_sizes = [max(len(i) for i in col) for col in cols]
+
+def pad(s, l):
+    return s + " " * (l - len(s))
+
+for row in zip(*cols):
+    for item, size in zip(row, col_sizes):
+        print(pad(item, size), end=" | ")
+    print()
+        
+con.close()
